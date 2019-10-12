eb4aaac222cc5f37840466cc74aaf69bf26128f5
djm0321 <djm0321@live.unc.edu>
2019-10-12 19:33:22 -0400

    Yelling at SQL

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 import pymysql
-conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='', db='')
+import pymysql.cursors
+conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='SuperStrongPWD17', db='userdata', cursorclass=pymysql.cursors.DictCursor)
 
 cur = conn.cursor()
-#sq = cur.execute("CREATE TABLE username (id INT NOT NULL AUTO_INCREMENT, user varchar(255), pass varchar(255) , money double, PRIMARY KEY (ID)  );")
+# cur.execute("CREATE DATABASE userdata")
+# sq = cur.execute("CREATE TABLE username (id INT NOT NULL AUTO_INCREMENT, user varchar(255), pwd varchar(255) , money double, PRIMARY KEY (ID)  );")
 val = "(0,'meterbeter5000','pigbeen','69')"
-cur.execute("INSERT INTO username (id,user,pass,money) VALUES (%s,%s)")
-
-tab = cur.execute("SELECT pass FROM username")
-print(tab)
+cur.execute("INSERT INTO username (id, user, pwd, money) VALUES (1, 'urmom', 'urMom', '69420')")
+# cur.execute("INSERT INTO username (id,user,pwd,money) VALUES (%s,%s)")
+cur.execute("""INSERT INTO username (id, user, pwd, money) VALUES (%s, %s, %s, %s))""" % (id, user, pwd, money))
+tab = cur.execute("SELECT * FROM username")
+rows = cur.fetchall()
+for row in rows:
+    print(row["user"], row["money"])

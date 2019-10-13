c47d83795f377eda746619fe95a65889bae90ab3
roddene <44795682+roddene@users.noreply.github.com>
2019-10-13 00:49:01 -0400

    Update yahoof.py

--- pullStocks/yahoof.py
+++ pullStocks/yahoof.py
@@ -1,20 +1,19 @@
 # Import yfinance
 import yfinance as yf
-from pandas.plotting import register_matplotlib_converters
-register_matplotlib_converters()
-# Get the data for the stock Apple by specifying the stock ticker, start date, and end date
-data = yf.download('MMM','2016-01-01','2017-01-01')
-
-# Plot the close prices
+from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-print(type(data))
-#print(data['Date'])
-#pd.to_datetime(data['Date'],format='%m/%d/%Y')
+# Get the data for the stock Apple by specifying the stock ticker, start date, and end date
+def getStock(stock,start,end):
+    data = yf.download(stock,start,end)
+    sns.set_style("darkgrid")
+    plt.style.use('dark_background')
 
-#data = data.set_index(pd.datetimeIndex(data['Date']))
+    plt.margins(x=0.01,y=0.01)
+    sns.lineplot(x=data.index, y="Close", data=data,color = "lightgreen",lw=2)#make graph
+    plt.ylabel("Value($)")
+    plt.title(stock + " from " + start + " to " + end)
+    plt.show()
 
-sns.lineplot(x=data.index, y="Close", data=data);
-plt.show()
-#pd.to_datetime(df['DATES'],format='%m/%d/%Y')
+getStock("MMM",'2016-01-01','2017-01-01')

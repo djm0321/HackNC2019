01861d5e96ce74502f0f42de9f17378c10cccbd9
roddene <44795682+roddene@users.noreply.github.com>
2019-10-13 02:24:18 -0400

    more

--- pullStocks/yahoof.py
+++ pullStocks/yahoof.py
@@ -4,16 +4,20 @@ from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
 # Get the data for the stock Apple by specifying the stock ticker, start date, and end date
 def getStock(stock,start,end):
-    data = yf.download(stock,start,end)
+    df = pd.read_csv('companies.csv',engine='python')
+    data = yf.download(df.iloc[stock,0],start,end)
     sns.set_style("darkgrid")
     plt.style.use('dark_background')
 
-    plt.margins(x=0.01,y=0.01)
+    plt.margins(x=0)
     sns.lineplot(x=data.index, y="Close", data=data,color = "lightgreen",lw=2)#make graph
     plt.ylabel("Value($)")
-    plt.title(stock + " from " + start + " to " + end)
+    plt.title(df.iloc[stock,1] + " from " + start + " to " + end)
     plt.show()
 
-getStock("MMM",'2016-01-01','2017-01-01')
+
+getStock(100,'2010-01-01','2019-01-01')
